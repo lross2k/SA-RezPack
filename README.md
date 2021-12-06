c5c858b346876f1a5cfefceb7d878bc86eb30cb8
RezmiCR <luisross87@gmail.com>
2021-12-06 14:25:20 -0600

    initial one

---
+++ README.md
@@ -0,0 +1,54 @@
+# SA-RezPack
+This collection includes multiple mods inteded to be easy to install to any vanilla GTA SA 
+game folder for versions 1.0 or Steam, many fixes and quality of life improvements, as well
+as dynamic lighting thanks to [gtaRenderHook](https://github.com/petrgeorgievsky/gtaRenderHook).
+
+## Installation
+***I recommend using a clean install of GTA SA*** to use this pack, however it should be possible 
+to install it to already modded games, but some problems might come up; **Not tested for the**
+**Rockstar Games Launcher version**.
+
+1. Download the release file.
+2. Make a backup of your Grand Theft Auto San Andreas folder (*recommended*).
+3. Unzip it to the game folder.
+4. Launch `gta-sa.exe` or your preferred launcher.
+
+## Mods included
+- [Mod Loader](https://www.gtagarage.com/mods/show.php?id=25377)
+- [Silent’s ASI Loader](https://cookieplmonster.github.io/mods/gta-sa/)
+- [GInput](https://cookieplmonster.github.io/mods/gta-sa/)
+- [SilentPatch](https://cookieplmonster.github.io/mods/gta-sa/)
+- [HD radar map](https://www.gtainside.com/en/sanandreas/mods/84409-san-andreas-hd-radar-map/)
+- [Mugetsuga’s fixes pack](https://www.mixmods.com.br/2018/06/mugetsugas-fixes-pack.html)
+- [Better leftovers fix](https://www.mixmods.com.br/2021/05/better-leftovers-fix.html)
+- [MixSets](https://www.mixmods.com.br/2019/08/mod-mixsets.html)
+- [Open limit adjuster](https://gtaforums.com/topic/736512-open-limit-adjuster/)
+- [SA mobile to PC](https://gtaforums.com/topic/661961-sa-mobile-to-pc-research-project/)
+- [CLEO library](http://cleo.li/)
+- [Cutscene characters for gameplay](https://www.mixmods.com.br/2020/05/sa-cutscene-characters-for-gameplay.html)
+- [SkyUI](https://www.mixmods.com.br/2017/11/SkyUI.html)
+- [Ragdoll physics](https://www.mixmods.com.br/2017/03/mod-ragdoll-bullet-physics-fisica-realista.html)
+- [HD intro movies](https://www.gtainside.com/en/sanandreas/mods/170782-hd-intro-movies-young-maylay-rap/)
+- [HD fonts](https://www.gtagarage.com/mods/show.php?id=20217)
+- [Autosave and mission restart](https://www.gtagarage.com/mods/show.php?id=14143)
+- [Vados HD peds pack](https://sharemods.com/fpzwm8yel0v0/GTA_SA_-_Vados_Peds_Pack_v2.0.7z.html)
+- [Upgraded vehicles pack 1.8](https://www.gtainside.com/en/sanandreas/cars/169770-upgraded-vehicles-pack-1-8-unofficial-fix/)
+- [PS2 timecyc](https://www.gtagarage.com/mods/show.php?id=20208)
+- [VehFuncs](https://github.com/JuniorDjjr/VehFuncs)
+- [Vanilla vehicle fixes](https://gtaforums.com/topic/725635-gta-san-andreas-fixed-vanilla-vehicles/)
+- [HD CJ textures](https://www.mixmods.com.br/2021/06/cj-upscale-hd-v2-0-textura-hd-para-o-player/)
+- [HD vegetation](https://www.gtagarage.com/mods/show.php?id=14994)
+- [HD loading screens](https://www.gtainside.com/en/sanandreas/mods/170824-hd-loadscreens-menu-cursor/)
+
+## Mods I'd like to add in the future
+In case someone would like to help me add more, **and test it before submitting merges**
+- [HD weapons](https://gtaforums.com/topic/933587-improved-default-armoury-reflections-positions-model-and-texture-improvements/)
+- [Map texture fixes](https://www.mixmods.com.br/2015/05/map-textures-fix-correcao-das-texturas.html)
+https://www.mixmods.com.br/2015/03/nodep-desativar-dep.html
+- First person camera
+
+## Why a github repo?
+Well, I would like to make this a smart installed, but that's for the future and just for 
+learning purposes.
+
+*Also the formatting would be prettier this way.*
---
+++ shaders/Deferred.hlsl
@@ -0,0 +1,244 @@
+#include "GameMath.hlsl"
+#include "Shadows.hlsl"
+#include "LightingFunctions.hlsl"
+#include "GBuffer.hlsl"
+#include "VoxelGI.hlsl"
+#include "Shadows.hlsl"
+//--------------------------------------------------------------------------------------
+// Variables
+//--------------------------------------------------------------------------------------
+Texture2D txGB0 	: register(t0);
+Texture2D txGB1 	: register(t1);
+Texture2D txGB2 	: register(t2);
+Texture2D txGB3     : register(t3);
+
+Texture2D txShadow 	: register(t4);
+Texture2D txLighting : register(t4);
+Texture2D txPrevFrame : register(t5);
+Texture2D txReflections : register(t6);
+Texture2D txAO : register(t7);
+#ifndef SAMLIN
+#define SAMLIN
+SamplerState samLinear : register(s0);
+#endif
+SamplerComparisonState samShadow          : register(s1);
+struct Light
+{
+	float3  vPosition;
+	int   nLightType;
+	float3  cColor;
+	float   fRange;
+    float3  vDir;
+    float   fPadding;
+};
+/*!
+    Matrices updated each frame
+*/
+cbuffer LightBuffer : register(b10)
+{
+    Light aDynamicLights[16];
+};
+struct VS_QUAD_IN
+{
+    float4 vPosition : POSITION;
+	float2 vTexCoord : TEXCOORD0;
+};
+struct PS_QUAD_IN
+{
+    float4 vPosition : SV_Position;
+    float4 vTexCoord : TEXCOORD0;
+};
+//--------------------------------------------------------------------------------------
+// Vertex Shader
+//--------------------------------------------------------------------------------------
+PS_QUAD_IN VS(VS_QUAD_IN i)
+{
+    PS_QUAD_IN o;
+    o.vPosition = i.vPosition;
+    o.vTexCoord = float4(i.vTexCoord, 0, 1);
+	return o;
+}
+
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+// Direction light pass
+float4 SunLightingPS(PS_QUAD_IN i) : SV_Target
+{
+	float4 OutLighting;
+
+    const float3 ViewPos = mViewInv[3].xyz;
+
+    clip(vSunLightDir.w <= 0.0f);
+
+    float4 AlbedoColor = txGB0.Sample(samLinear, i.vTexCoord.xy);
+
+	clip(length(AlbedoColor) <= 0);
+
+	float3 Normals;
+	float ViewZ;
+    GetNormalsAndDepth(txGB1, samLinear, i.vTexCoord.xy, ViewZ, Normals);
+    Normals = normalize(Normals);
+    float4 Parameters = txGB2.Sample(samLinear, i.vTexCoord.xy);
+    float4 Radiance = txGB3.Sample(samLinear, i.vTexCoord.xy);
+
+	float Roughness = 1.1 - Parameters.y;
+    float SpecIntensity = Parameters.x;
+
+	float Luminance = dot(AlbedoColor.rgb, float3(0.2126, 0.7152, 0.0722));
+
+	
+	float3 WorldPos	= DepthToWorldPos(ViewZ, i.vTexCoord.xy).xyz;
+	
+	float3 ViewDir = normalize(WorldPos.xyz - ViewPos); // View direction vector
+	
+	float Distance = length(WorldPos.xyz - ViewPos);
+	
+	
+	float DiffuseTerm, SpecularTerm;
+	CalculateDiffuseTerm_ViewDependent(Normals, vSunLightDir.xyz, ViewDir, DiffuseTerm, Roughness);
+	CalculateSpecularTerm(Normals, vSunLightDir.xyz, -ViewDir, Roughness, SpecularTerm);
+
+#if SAMPLE_SHADOWS==1
+    float ShadowTerm = SampleShadowCascades(txShadow, samShadow, samLinear, WorldPos, Distance) * vSunLightDir.w;
+#else
+    float ShadowTerm = 1.0;
+#endif
+
+    float2 Lighting = float2(DiffuseTerm, SpecularTerm * SpecIntensity) * ShadowTerm;
+    OutLighting.xyzw = float4(Lighting.x * vSunColor.rgb + Radiance.rgb * saturate(1.0f - vSunLightDir.w + 0.2f), Lighting.y);
+	
+	return OutLighting;
+}
+
+float SpotLightIntensity(float cosA, float umbraAngle, float penumbraAngle)
+{
+    float t = saturate((cosA - cos(umbraAngle)) / (cos(penumbraAngle) - cos(umbraAngle)));
+    return smoothstep(0, 1, t);
+}
+
+// point and spot light pass
+float4 PointLightingPS(PS_QUAD_IN i) : SV_Target
+{
+	float4 OutLighting;
+    float3 Normals;
+    float ViewZ;
+
+    const float3 ViewPos = mViewInv[3].xyz;
+
+    float4 AlbedoColor = txGB0.Sample(samLinear, i.vTexCoord.xy);
+	clip(AlbedoColor.a <= 0);
+    
+    float4 Parameters = txGB2.Sample(samLinear, i.vTexCoord.xy);
+
+    GetNormalsAndDepth(txGB1, samLinear, i.vTexCoord.xy, ViewZ, Normals);
+    Normals = normalize(Normals);
+
+    float3 WorldPos = DepthToWorldPos(ViewZ, i.vTexCoord.xy).xyz;
+	
+	float3 ViewDir = normalize(WorldPos.xyz - ViewPos); // View direction vector
+	
+	float Distance = length(WorldPos.xyz - ViewPos);
+
+	float Luminance = dot(AlbedoColor.rgb, float3(0.2126, 0.7152, 0.0722));
+	float Roughness = 1 - Parameters.y;
+	float SpecIntensity = Parameters.x;
+
+	float3 FinalDiffuseTerm = float3(0, 0, 0);
+	float FinalSpecularTerm = 0;
+    //[unroll(128)]
+    for (uint i = 0; i < uiLightCount; i++)
+	{
+        float3 LightDir = -normalize(WorldPos.xyz - aDynamicLights[i].vPosition.xyz);
+        float LightDistance = length(WorldPos.xyz - aDynamicLights[i].vPosition.xyz);
+
+        float DiffuseTerm, SpecularTerm;
+        float3 LightColor = aDynamicLights[i].cColor;
+		CalculateDiffuseTerm_ViewDependent(Normals.xyz, LightDir, ViewDir, DiffuseTerm, Roughness);
+		CalculateSpecularTerm(Normals.xyz, LightDir, -ViewDir, Roughness, SpecularTerm);
+		
+        float Attenuation = 1.0f - pow(saturate(LightDistance / aDynamicLights[i].fRange), 2);
+        Attenuation *= Attenuation;
+        if (aDynamicLights[i].nLightType == 1)
+		{
+            float fSpot = SpotLightIntensity(max(dot(-LightDir, aDynamicLights[i].vDir), 0.0f), PI / 3.0f, PI / 6.0f); //pow(, 4.0f);
+            Attenuation *= fSpot;
+        }
+			
+        FinalDiffuseTerm += DiffuseTerm * Attenuation * LightColor;
+        FinalSpecularTerm += SpecularTerm * Attenuation * SpecIntensity;
+    }
+	float4 Lighting = float4(FinalDiffuseTerm, FinalSpecularTerm);
+	OutLighting.xyzw = Lighting;
+	
+	return OutLighting;
+}
+
+float4 BlitPS(PS_QUAD_IN i) : SV_Target
+{
+	return txPrevFrame.Sample(samLinear, i.vTexCoord.xy);
+}
+
+float4 FinalPassPS(PS_QUAD_IN i) : SV_Target
+{
+	float4 OutLighting;
+    const float3 ViewPos = mViewInv[3].xyz;
+
+    float4 AlbedoColor = txGB0.Sample(samLinear, i.vTexCoord.xy);
+    float4 Parameters = txGB2.Sample(samLinear, i.vTexCoord.xy);
+    float3 Normals;
+    float ViewZ;
+    GetNormalsAndDepth(txGB1, samLinear, i.vTexCoord.xy, ViewZ, Normals);
+    Normals = normalize(Normals);
+
+    float3 WorldPos = DepthToWorldPos(ViewZ, i.vTexCoord.xy).xyz;
+
+    float3 ViewDir = normalize(WorldPos.xyz - ViewPos);
+    uint MaterialType = ConvertToMatType(Parameters.w);
+	
+	float Metallness = Parameters.x;
+
+    float4 Lighting = txLighting.Sample(samLinear, i.vTexCoord.xy);
+	
+	if (AlbedoColor.a <= 0)
+	{
+		OutLighting.xyz = float3(0, 0, 0);
+		OutLighting.a = 0;
+	}
+	else
+	{
+        float AmbientOcclusion = txAO.Sample(samLinear, i.vTexCoord.xy).r;
+		// Diffuse term consists of diffuse lighting, and sky ambient
+        float3 DiffuseTerm = (Lighting.xyz + vSkyLightCol.rgb * 1.0f * AmbientOcclusion);
+		// Specular term consists of specular highlights
+        float3 SpecularTerm = (Lighting.w * Lighting.xyz) * AmbientOcclusion;
+		// Reflection term is computed before deferred
+		float3 ReflectionTerm = txReflections.Sample(samLinear, i.vTexCoord.xy).rgb;
+        // Fresnel coeff
+        float FresnelCoeff = MicrofacetFresnel(Normals, -ViewDir, Parameters.y);
+        // Increase reflection for cars
+		switch (MaterialType) /* As seen in TBP shaders */
+        {
+        // Cars
+        case 1:
+            DiffuseTerm	= Lighting.xyz + 0.6f * AmbientOcclusion * AlbedoColor.rgb;
+            FresnelCoeff = lerp(0.0f, FresnelCoeff, Parameters.y);
+            ReflectionTerm *= 6.0f;
+            break;
+        // Skins
+        case 2:
+            DiffuseTerm	= Lighting.xyz + 0.4f * AmbientOcclusion * AmbientOcclusion;
+			ReflectionTerm = 0.0f;
+			FresnelCoeff = 0.0f;
+            break;
+        // Other stuff, I couln't find anything else that was special
+        default:
+            ReflectionTerm *= 0.5f;
+            break;
+        }
+		// Add atmospheric scattering to result
+        OutLighting.xyz = DiffuseTerm * AlbedoColor.rgb + SpecularTerm * Parameters.x + ReflectionTerm * FresnelCoeff * Parameters.x;
+		OutLighting.a = 1;
+	}
+	return OutLighting;
+}
